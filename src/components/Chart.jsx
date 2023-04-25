f0dfe1eb203a9ce7e5e3d068de3f62f1c453668e
meryiir <mariemboujja14@gmail.com>
2023-04-25 13:34:44 +0100

    calender

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,17 +1,73 @@
-import { Card, Text, Metric, Flex, ProgressBar } from "@tremor/react"
-import React from 'react'
+import * as React from "react";
+import { PieChart } from "react-minimal-pie-chart"
+
 
 const Chart = () => {
+
+
+
+
+    const data = [
+        {
+            "name": "Group A",
+            "value": 400
+        },
+        {
+            "name": "Group B",
+            "value": 300
+        },
+        {
+            "name": "Group C",
+            "value": 500
+        },
+        {
+            "name": "Group D",
+            "value": 200
+        },
+        {
+            "name": "Group E",
+            "value": 278
+        },
+        {
+            "name": "Group F",
+            "value": 189
+        }
+    ]
+
     return (
-        <Card className="max-w-xs mx-auto">
-            <Text>Sales</Text>
-            <Metric>$ 71,465</Metric>
-            <Flex className="mt-4">
-                <Text>32% of annual target</Text>
-                <Text>$ 225,000</Text>
-            </Flex>
-            <ProgressBar percentageValue={32} className="mt-2" />
-        </Card>
+        <div className="App">
+            {/*<h1>Charts students</h1> */}
+
+            <PieChart
+                style={{ height: 450, fontFamily: "monospace", FontSize: 16, marginTop: 100}}
+                data={[
+                    { name: 'passed students', value: 20, color: 'green' },
+                    { name: 'failed students', value: 5, color: 'red' },
+                ]}
+                label={({ dataEntry }) => `${Math.round(dataEntry.percentage)} %`}
+                labelPosition="50"
+                animate={true}
+
+            ></PieChart>
+
+            <div className="charts" style={{ display: "flex", justifyContent: "center", marginTop: 30 }}>
+                <ul>
+                    <li style={{ color: "green", display: "flex", flexDirection: "row",  gap: 10 }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-circle-fill" viewBox="0 0 16 16">
+                            <circle cx="8" cy="8" r="8" />
+                        </svg>
+                        Passed students
+                    </li>
+                    
+                    <li style={{ color: "red", display: "flex", flexDirection: "row" ,  gap: 10 }}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-circle-fill" viewBox="0 0 16 16">
+                            <circle cx="8" cy="8" r="8" />
+                        </svg>
+                    Failed students</li>
+                </ul>
+            </div>
+
+        </div>
     )
 }
 
--- src/components/Statistiques.jsx
+++ src/components/Statistiques.jsx
@@ -13,4 +13,5 @@ const Statistiques = () => {
   )
 }
 
+
 export default Statistiques
