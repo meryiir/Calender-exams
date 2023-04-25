56e2a933c589868d5f7249801ca4cb144ec08246
meryiir <mariemboujja14@gmail.com>
2023-04-25 15:39:43 +0100

    calendar

--- src/components/Profil.jsx
+++ src/components/Profil.jsx
@@ -1,14 +1,94 @@
 import React from 'react'
 import Input from './Input'
 import Navbar from './Navbar'
+import {RiLockPasswordFill} from "react-icons/ri"
+import {CgProfile} from "react-icons/cg"
 
 const Profil = () => {
   return (
-    
+    <>
       <Navbar />
 
-     
+      <>
+      <div className="min-h-screen flex flex-col max-w-md mx-auto bg-gray-200 opacity-100 font-poppins px-4 bg-no-repeat bg-cover bg-center">
+        
+        <div className="flex items-center px-4 pt-12 justify-between">
+          <div className="w-24 h-24 bg-white-600 flex items-center rounded-full">
+            <CgProfile style={{ height : 70 , width : 70 , marginLeft : 20}}/>
+          </div>
+          <div className="w-9/12 flex items-center">
+            <div className="w-10/12 flex flex-col leading-none pl-4">
+              <p className="text-2xl font-bold">Ahmed</p>
+              <p className="text-sm pt-1 font-light text-gray-700">
+                Teacher
+              </p>
+            </div>
+            <div className="w-2/12">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="text-gray-700"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  width={24}
+                  height={24}
+                >
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path d="M9.243 19H21v2H3v-4.243l9.9-9.9 4.242 4.244L9.242 19zm5.07-13.556l2.122-2.122a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414l-2.122 2.121-4.242-4.242z" />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="pt-12 px-4 w-full flex flex-col">
+          <p className="font-semibold text-gray-600">My Personnal informations</p>
+          <div className="flex w-full pt-2 space-x-2">
+           
+            <button className="bg-green-800 w-96 rounded-full px-4 py-2 font-ligth text-white flex">
+                Number of Groupes :
+            </button>
+           
+          </div>
+
+          <button className="bg-red-800 w-96 rounded-full px-4 py-2 font-ligth text-white flex mt-8">
+          Modul :
+      </button>
+
+          <button className="bg-red-800 w-96 rounded-full px-4 py-2 font-ligth text-white flex mt-8 ">
+                Email :
+            </button>
+        </div>
+        <div className="pt-12 px-4 w-full flex flex-col">
+          <p className="font-semibold text-gray-600">Dashboard</p>
+          <div className="flex flex-col w-full pt-2 space-y-2">
+            <div className="flex w-full h-12">
+              <div className="w-2/12 h-full">
+              <RiLockPasswordFill style={{height : 50 , width : 40}}/ >
+              </div>
+              <div className="w-6/12 h-full flex items-start">
+              
+                <p className="my-auto text-lg font-semibold">Change Your Password</p>
+              </div>
+              <div className="w-4/12 h-full flex justify-end items-end">
+                <button className="flex bg-red-600 rounded-md my-auto px-5 py-1 float-right text-white font-medium">
+            
+                  Reset
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <style
+        dangerouslySetInnerHTML={{
+          __html:
+            "\n.bg-gelap{\n  background-color: #001E26;\n}\n.bg-btn{\n  background-color: #FF6446;\n}\n"
+        }}
+      />
+    </>
     
+    </>
+
   )
 }
 
--- src/components/Reclamations.jsx
+++ src/components/Reclamations.jsx
@@ -1,12 +1,22 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 
 const Reclamations = () => {
   return (
     <>
-      <Navbar/>
-      Reclamations
+      <Navbar />
+
+      <label for="message" class="block mb-2 text-sm font-medium mt-8 text-gray-900 dark:text-green" style={{fontWeight: "bold" , fontSize: 20}}>Justification</label>
+      <textarea id="message" rows="8" class="block p-2.5 w-full text-sm mt-8 text-white-900 bg-grey-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="What doesn't satisfy you ?"></textarea>
+      <button type="submit" class="inline-flex items-center py-2.5 px-4 m-3 text-xs font-medium text-center text-white bg-gray-600 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-gray-900">
+        Send Request
+      </button>
+
+      <p style={{ color: "red", fontWeight: "bold" }} className="ml-5">
+        Please send your request within 72 hours.
+      </p>
+
+
     </>
   )
 }
